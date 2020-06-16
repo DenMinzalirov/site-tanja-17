d64873e14e5fadda5904942e13c766d0d65073b5
DenMinzalirov <den.minzakirov@gmail.com>
2020-06-17 01:37:10 +0300

    2

---
+++ precache-manifest.105c2aeda7becbd9b645aaf3b7a40872.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22857893d4376e342a2d83c49b0fac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86d0f879f8616fdb631e",
+    "url": "/static/css/2.9f2bb942.chunk.css"
+  },
+  {
+    "revision": "eb0ada029b0a3ee7a8f8",
+    "url": "/static/css/main.967144da.chunk.css"
+  },
+  {
+    "revision": "86d0f879f8616fdb631e",
+    "url": "/static/js/2.c4ca3b07.chunk.js"
+  },
+  {
+    "revision": "ba5c44dd1ae0cf4a36ce821f0b09a39e",
+    "url": "/static/js/2.c4ca3b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb0ada029b0a3ee7a8f8",
+    "url": "/static/js/main.d65edf6d.chunk.js"
+  },
+  {
+    "revision": "cc1c26e454abbd6be578",
+    "url": "/static/js/runtime-main.ac793b4f.js"
+  },
+  {
+    "revision": "4070a4de6a3113a2963d7dab08cc76bd",
+    "url": "/static/media/19516.4070a4de.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6fd266c33150190422b78ff6aee13062.js"
+  "/precache-manifest.105c2aeda7becbd9b645aaf3b7a40872.js"
 );
 
 self.addEventListener('message', (event) => {
